0104a6ad916a2ff52d2b94c8c961742bfad95007
Damian Baćkowski <damianbackowski@gmail.com>
2017-01-04 19:23:57 +0100

    add toast messages and loading screen

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, ToastController } from 'ionic-angular';
 
 import { User } from './../../models/user';
 import { Users } from './../../providers/users';
@@ -18,7 +18,12 @@ import { UserDetailsPage } from './../user-details/user-details';
 export class UsersPage {
   users: User[]
 
-  constructor(public navCtrl: NavController, private Users: Users) {}
+  constructor(
+    public navCtrl: NavController, 
+    private Users: Users, 
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController
+  ) {}
 
   ionViewDidLoad() {
     console.log('Hello UsersPage Page');
@@ -32,9 +37,27 @@ export class UsersPage {
     let term = searchEvent.target.value
 
     if (term.trim().length >= 2) {
-      this.Users.searchUsers(term).subscribe(users => {
-        this.users = users
+      let loading = this.loadingCtrl.create({
+        content: 'Please wait...'
       });
+
+      loading.present();
+
+      this.Users.searchUsers(term).subscribe(
+        users => {
+          this.users = users
+          loading.dismiss();
+        },
+        error => {
+          this.toastCtrl.create({
+            message: 'API response error.',
+            duration: 3000,
+            position: 'bottom'
+          }).present();
+
+          loading.dismiss();
+        }
+      );
     }
   }
 }
